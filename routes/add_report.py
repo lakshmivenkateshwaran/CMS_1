6255e0ee07aef7550991ea52f6c82f5e902e06c3
Lakshmivenkateshwaran <vvenkivasudevan9@gmail.com>
2025-05-01 13:51:32 +0530

    gitignore is added

--- routes/add_report.py
+++ routes/add_report.py
@@ -19,10 +19,10 @@ from sqlalchemy import func
 from datetime import datetime, date, timedelta
 from databases.database import get_db
 from services.addreport_services import get_filtered_data, get_date_range_from_type
-from services.email_util import send_email_report
+from services.email_util import send_email_reportb 
 from models.chart_model import ChartType
 from models.category_model import Category
-from models.sub_category_model import SubCategory
+from models.sub_category_model import SubCategory    
 from models.product_model import Product
 from models.manufacturer_model import Manufacturer
 from models.crawl_websites import CrawlingWebsiteLog
@@ -52,50 +52,49 @@ from security.security import get_current_user_id
 
 router = APIRouter()
 
-@router.get("/report-data")
-def get_report_data_all(
-    report_name: str = Query(None),
-    chart_type: str = Query(None),
-    category: str = Query(None),
-    subcategory: str = Query(None),
-    product: str = Query(None),
-    manufacturer: str = Query(None),
-    db: Session = Depends(get_db)
-):
-    filters = {
-        "report_name": report_name,
-        "chart_type_id": chart_type,
-        "category": category,
-        "subcategory": subcategory,
-        "product": product,
-        "manufacturer": manufacturer
-    }
-    data = get_filtered_data(db, filters)
-    return {"data": data}
+# @router.get("/report-data")
+# def get_report_data_all(
+#     report_name: str = Query(None),
+#     chart_type: str = Query(None),
+#     category: str = Query(None),
+#     subcategory: str = Query(None),
+#     product: str = Query(None),
+#     manufacturer: str = Query(None),
+#     db: Session = Depends(get_db)
+# ):
+#     filters = {
+#         "report_name": report_name,
+#         "chart_type_id": chart_type,
+#         "category": category,
+#         "subcategory": subcategory,
+#         "product": product,
+#         "manufacturer": manufacturer
+#     }
+#     data = get_filtered_data(db, filters)
+#     return {"data": data}
 
-#  Dropdown endpoints
-@router.get("/chart-types")
-def get_chart_types_dropdown(db: Session = Depends(get_db)):
-    return db.query(ChartType).all()
+# #  Dropdown endpoints
+# @router.get("/chart-types")
+# def get_chart_types_dropdown(db: Session = Depends(get_db)):
+#     return db.query(ChartType).all()
 
-@router.get("/categories")
-def get_categories_dropdown(db: Session = Depends(get_db)):
-    return db.query(Category).all()
+# @router.get("/categories")
+# def get_categories_dropdown(db: Session = Depends(get_db)):
+#     return db.query(Category).all()
 
-@router.get("/subcategories")
-def get_subcategories_dropdown(category_id: int, db: Session = Depends(get_db)):
-    return db.query(SubCategory).filter(SubCategory.category_id == category_id).all()
+# @router.get("/subcategories")
+# def get_subcategories_dropdown(category_id: int, db: Session = Depends(get_db)):
+#     return db.query(SubCategory).filter(SubCategory.category_id == category_id).all()
 
-@router.get("/products")
-def get_products_dropdown(subcategory_id: int, db: Session = Depends(get_db)):
-    return db.query(Product).filter(Product.subcategory_id == subcategory_id).all()
+# @router.get("/products")
+# def get_products_dropdown(subcategory_id: int, db: Session = Depends(get_db)):
+#     return db.query(Product).filter(Product.subcategory_id == subcategory_id).all()
 
-@router.get("/manufacturers")
-def get_manufacturers_dropdown(product_id: int, db: Session = Depends(get_db)):
-    return db.query(Manufacturer).filter(Manufacturer.product_id == product_id).all()
+# @router.get("/manufacturers")
+# def get_manufacturers_dropdown(product_id: int, db: Session = Depends(get_db)):
+#     return db.query(Manufacturer).filter(Manufacturer.product_id == product_id).all()
 
 # crawl website data 
-
 @router.get("/website-crawled-summary", response_model=list[WebsiteCrawlSummary])
 def get_today_website_crawled_summary(db: Session = Depends(get_db)):
     today_date = int(datetime.now().strftime("%Y%m%d"))
