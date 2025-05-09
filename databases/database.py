d485badd6970c8a214d4c6c9e740652b169d3b0a
Lakshmivenkateshwaran <vvenkivasudevan9@gmail.com>
2025-05-09 17:31:27 +0530

    Database url changed as separate

--- databases/database.py
+++ databases/database.py
@@ -3,11 +3,23 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 
 # Load environment variables
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# Get the database connection components from the environment variables
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+
+# URL encode the password
+encoded_password = quote_plus(db_password)
+
+#DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = f"mysql+pymysql://{db_user}:{encoded_password}@{db_host}:{db_port}/{db_name}"
 
 # Database connection
 engine = create_engine(DATABASE_URL)
